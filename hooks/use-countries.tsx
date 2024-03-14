fe451f6e402e89ccfb6c774d2c0f0e8294ffcdfd
Thiago <tmg3.mn@gmail.com>
2024-03-14 13:45:32 -0300

    Refact: Countries interface

--- hooks/use-countries.tsx
+++ hooks/use-countries.tsx
@@ -8,6 +8,18 @@ export interface CountryProps {
   population: number;
   region: string;
   capital: string;
+  nativeName: string;
+  subregion: string;
+  topLevelDomain: string[];
+  currencies: {
+    code: string;
+    name: string;
+    symbol: string;
+  }[];
+  borders: string[];
+  languages: {
+    name: string;
+  }[];
 }
 
 export const useCountries = () => {
